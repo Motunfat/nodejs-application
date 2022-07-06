8f5e2d07331ed2b52f4b503c669bc0725da4fa99
Motunfat <motunfat25@gmail.com>
2022-07-05 20:05:41 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -7,7 +7,7 @@ sonarqubeScanner({
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
 	    'sonar.login': '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',
-	    //'sonar.password': 'admin',
+	    //'sonar.password': 'Mercury@84',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
